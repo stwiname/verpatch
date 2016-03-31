dbe7847749bb950e435cf695777c240107018786
Scott <skott.twiname@gmail.com>
2016-03-31 17:54:35 +1300

    Improve tests

--- test/test.js
+++ test/test.js
@@ -1,38 +1,35 @@
 var verpatch = require('../lib/index');
-const expect = require('chai').expect;
-const fs = require('fs'),
-  path = require('path'),
-  bluebird = require('bluebird');
 
+const bluebird = require('bluebird'), 
+  fs = bluebird.promisifyAll(require('fs')),
+  path = require('path'),
+  expect = require('chai').expect
+  ;
+
+bluebird.onPossiblyUnhandledRejection(function(){});
 
 describe('verpatch', function(){
   this.timeout(5000);
   it('throws an error when not passed an executable path', () => {
-     return verpatch()
-      .catch(error => {
-        expect(error).not.to.be.null;
-      });
+    return expect(verpatch()).to.be.rejected;
   });
 
   it('throws an error when not passed options', () => {
-    return verpatch('./path/to/exe')
-      .catch(error => {
-        expect(error).not.to.be.null;
-      });
+    return expect(verpatch('./path/to/exe')).to.be.rejected;
   });
 
   it('should be able to modify version information of an exe', () => {
-
+    const executablePath = path.resolve(__dirname, '../bin/verpatch_copy.exe');
 
     return new bluebird((resolve, reject) => {
       fs.createReadStream(path.resolve(__dirname,'../bin/verpatch.exe'))
-        .pipe(fs.createWriteStream(path.resolve(__dirname, '../bin/verpatch_copy.exe')))
+        .pipe(fs.createWriteStream(executablePath))
         .on('error', reject)
         .on('finish', resolve);
     })
     .then(() => {
       return verpatch(
-        path.resolve(__dirname, '../bin/verpatch_copy.exe')
+          executablePath
         , {
             version: '1.0.0'
           , desc:    'description'
@@ -42,7 +39,34 @@ describe('verpatch', function(){
           , product: 'product name'
           , '(c)':   'copyright'
         });
+    })
+    .then(() => {
+      //Cleanup copy
+      return fs.unlinkAsync(executablePath);
     });
-
   });
+
+  it('should fail trying to set an invalid option', () => {
+    const executablePath = path.resolve(__dirname, '../bin/verpatch_copy.exe');
+
+    return new bluebird((resolve, reject) => {
+      fs.createReadStream(path.resolve(__dirname,'../bin/verpatch.exe'))
+        .pipe(fs.createWriteStream(executablePath))
+        .on('error', reject)
+        .on('finish', resolve);
+    })
+    .then(() => {
+      expect(verpatch(
+          executablePath
+        , {
+            invalid: 'invalid option'
+        })).to.be.rejected;
+    })
+    .then(() => {
+      //Cleanup copy
+      return fs.unlinkAsync(executablePath);
+    });
+  });
+
+
 });
